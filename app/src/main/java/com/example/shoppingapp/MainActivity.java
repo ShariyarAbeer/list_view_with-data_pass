280d286652ff67bd754a194fe90829d3e7dc2753
ShariyarAbeer <007abeershariyar@gmail.com>
2021-02-24 16:22:35 +0600

    list view from local done

--- app/src/main/java/com/example/shoppingapp/MainActivity.java
+++ app/src/main/java/com/example/shoppingapp/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.shoppingapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listview);
+        String[] listName = new String[] {"abeer","tanvire", "mahinoor","tanvire", "mahinoor","tanvire", "mahinoor","tanvire", "mahinoor","tanvire", "mahinoor","tanvire", "mahinoor","tanvire", "mahinoor","tanvire", "mahinoor","tanvire", "mahinoor","tanvire", "mahinoor","tanvire", "mahinoor","tanvire", "mahinoor","tanvire", "mahinoor","tanvire", "mahinoor","tanvire", "mahinoor","tanvire", "mahinoor","tanvire", "mahinoor","tanvire", "mahinoor","tanvire", "mahinoor","tanvire", "mahinoor","tanvire", "mahinoor","tanvire", "mahinoor","tanvire", "mahinoor","tanvire", "mahinoor","tanvire", "mahinoor","tanvire", "mahinoor","tanvire", "mahinoor","tanvire", "mahinoor","tanvire", "mahinoor","tanvire", "mahinoor","tanvire", "mahinoor","tanvire", "mahinoor","tanvire", "mahinoor","tanvire", "mahinoor","tanvire", "mahinoor","tanvire", "mahinoor"};
+
+        ArrayAdapter <String> arrayAdapter = new ArrayAdapter<String >(MainActivity.this,R.layout.item_view,R.id.textView,listName);
+        listView.setAdapter(arrayAdapter);
+
+
+
+
+
     }
 }
